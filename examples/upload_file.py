571a92d2f1b263bc3f15157e25ef07e1bab64533
Łukasz Faruga <lukasz.faruga1@gmail.com>
2024-05-29 02:11:00 +0200

    add upload file example

---
+++ examples/upload_file.py
@@ -0,0 +1,13 @@
+import requests
+
+# Assuming your endpoint is hosted at http://example.com
+url = "http://127.0.0.1:8000/files/upload"
+filepath = "./download_file.py"
+
+# Assuming you have a file named "example.txt" in the same directory as your script
+files = {'file': open(filepath, 'rb')}
+
+response = requests.post(url, files=files)
+
+# Print the response from the server
+print(response.json())
