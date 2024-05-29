e98aa47bd9d1cfb76cda3ed8ae1289268e9e40af
Bartosz Bartoszewski <bartoszewski@student.agh.edu.pl>
2024-05-29 13:20:49 +0200

    chore: Add lazy loading for product images

--- app/controllers/admin/add_product/collect_product.py
+++ app/controllers/admin/add_product/collect_product.py
@@ -22,7 +22,7 @@ class Product(BaseModel):
 
 app = FastAPI()
 base_url = "http://api:8000"
-
+photos_url = "http://api:8000/files/upload"
 
 @app.post("/products/")
 async def create_product(
@@ -33,65 +33,21 @@ async def create_product(
     cena_zakupu: float = Form(...),
     data_wprowadzenia: str = Form(...),
     zdjecie: UploadFile = File(...)
-):
-    
+): 
+    files = {'file': zdjecie.file}
+    response1 = requests.post(photos_url, files=files)
+    # print(response1.json()['file_id'])
     product = Product(
         name = nazwa,
         description = opis,
         sale_price = cena_sprzedazy,
         quantity = ilosc_dostepnych_sztuk,
         buy_price = cena_zakupu,
-        date = 'asd',
-        picture_path = 'default_path'
+        date = 'up to dane',
+        picture_path = response1.json()['file_id']
     )
-    response = requests.post(f"{base_url}/products/", json=product.dict())
-    print(response.json())
+    
+    response2 = requests.post(f"{base_url}/products/", json=product.dict())
+    
 
     return JSONResponse(content={"message": "Produkt został zapisany pomyślnie"}, status_code=200)
-
-
-
-
-# from fastapi import FastAPI, Form, UploadFile, File
-# from fastapi.responses import JSONResponse
-# import os
-# from datetime import datetime
-# import shutil
-
-# app = FastAPI()
-
-# @app.post("/products/")
-# async def create_product(
-#     nazwa: str = Form(...),
-#     opis: str = Form(...),
-#     cena_sprzedazy: float = Form(...),
-#     ilosc_dostepnych_sztuk: int = Form(...),
-#     cena_zakupu: float = Form(...),
-#     data_wprowadzenia: str = Form(...),
-#     zdjecie: UploadFile = File(...)
-# ):
-#     os.makedirs('products', exist_ok=True)
-#     product_id = datetime.now().strftime("%Y%m%d%H%M%S")
-#     product_data = {
-#         'nazwa': nazwa,
-#         'opis': opis,
-#         'cena_sprzedazy': cena_sprzedazy,
-#         'ilosc_dostepnych_sztuk': ilosc_dostepnych_sztuk,
-#         'cena_zakupu': cena_zakupu,
-#         'data_wprowadzenia': data_wprowadzenia
-#     }
-
-#     # Zapis danych produktu do pliku
-#     with open(f'products/{product_id}.txt', 'w') as f:
-#         for key, value in product_data.items():
-#             f.write(f"{key}: {value}\n")
-    
-#     # Zapis zdjęcia produktu
-#     with open(f'products/{product_id}_{zdjecie.filename}.png', 'wb') as buffer:
-#         shutil.copyfileobj(zdjecie.file, buffer)
-
-#     return JSONResponse(content={"message": "Produkt został zapisany pomyślnie"}, status_code=200)
-
-# # if __name__ == "__main__":
-# #     import uvicorn
-# #     uvicorn.run(app, host="127.0.0.1", port=8000)
