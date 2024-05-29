a62559a3b73e83988fae519352618ce1b2eec581
Bartosz Bartoszewski <bartoszewski@student.agh.edu.pl>
2024-05-29 11:40:03 +0200

    product update

--- app/databases/api/src/models/product.py
+++ app/databases/api/src/models/product.py
@@ -1,22 +1,49 @@
 import nanoid
 from typing import List
 from pydantic import BaseModel, Field
-
+from datetime import datetime
+from pathlib import Path
 
 class Product(BaseModel):
     id: str = Field(default_factory=lambda: nanoid.generate(size=10))
     name: str
-    price: float
+    description: str = "default description"
+    sale_price: float = 0
     quantity: int = 0
-    description: str = ""
-    category: str
-    tags: List[str] = []
+    buy_price: float = 0
+    date: str
+    picture_path: str
 
 
 class ProductUpdate(BaseModel):
     name: str | None = None
-    price: float | None = None
-    quantity: int | None = None
-    description: str | None = None
-    category: str | None = None
-    tags: List[str] | None = None
+    description: str = "default description"
+    sale_price: float = 0
+    quantity: int = 0
+    buy_price: float = 0
+    date: str | None = None
+    picture_path: str | None = None
+
+
+# import nanoid
+# from typing import List
+# from pydantic import BaseModel, Field
+
+
+# class Product(BaseModel):
+#     id: str = Field(default_factory=lambda: nanoid.generate(size=10))
+#     name: str
+#     price: float
+#     quantity: int = 0
+#     description: str = ""
+#     category: str
+#     tags: List[str] = []
+
+
+# class ProductUpdate(BaseModel):
+#     name: str | None = None
+#     price: float | None = None
+#     quantity: int | None = None
+#     description: str | None = None
+#     category: str | None = None
+#     tags: List[str] | None = None
