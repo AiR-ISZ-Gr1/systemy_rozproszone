e4015c14eba004b908cbb11b8f633b533a3cded8
Bartosz Bartoszewski <bartoszewski@student.agh.edu.pl>
2024-05-31 00:50:48 +0200

    refactor: Improve product editing functionality

--- app/frontend/pages/update_product_admin.py
+++ app/frontend/pages/update_product_admin.py
@@ -28,45 +28,47 @@ st.title("Shop Management System")
 response = requests.get(f"{api_url}/products")
 products = response.json()
 
-# Select a product to edit
-product_ids = [product["id"] for product in products]
-selected_product_id = st.selectbox("Choosen product", product_ids, index=None, placeholder="Select a product to edit")
+joined_list = {product["name"]: product["id"] for product in products}
 
-if selected_product_id:
-    response = requests.get(f"{api_url}/products/{selected_product_id}")
-    selected_product = response.json()
 
-    st.write("### Edit Product")
-    
-    name = st.text_input("Name", selected_product["name"])
-    description = st.text_area("Description", selected_product["description"])
-    sell_price = st.number_input("Sell price", value=selected_product["sell_price"])
-    quantity = st.number_input("Quantity", value=selected_product["quantity"])
-    buy_price = st.number_input("Buy price", value=selected_product["buy_price"])
-    tags = st.multiselect("Categories", ["Flower", "Tree", "Object", "Other", "Manure"], selected_product["tags"])
-    image_show = show_photo(selected_product["image_id"])
-    if image_show:
-        st.image(image_show)
-    image = st.file_uploader("Image", type=['jpg', 'jpeg', 'png', 'JPG', 'JPEG', 'PNG'])
-    
-    if st.button("Update Product"):
-        updated_product = Product(
-            name=name,
-            description=description,
-            sell_price=sell_price,
-            quantity=quantity,
-            buy_price=buy_price,
-            tags=tags
-        )
+selected_product_name = st.selectbox("Choosen product", joined_list, index=None, placeholder="Select a product to edit")
+if selected_product_name is not None:
+    selected_product_id = joined_list[selected_product_name]
+    if selected_product_id:
+        response = requests.get(f"{api_url}/products/{selected_product_id}")
+        selected_product = response.json()
 
-        if image is None:
-            image_id = selected_product["image_id"]
-        else:
-            updated_product.add_product_image(image)
-            
-        response = requests.put(f"{api_url}/products/{selected_product_id}", json=updated_product.dict())
+        st.write("### Edit Product")
         
-        if response.status_code == 200:
-            st.success("Product updated successfully")
-        else:
-            st.error("Failed to update product")
+        name = st.text_input("Name", selected_product["name"])
+        description = st.text_area("Description", selected_product["description"])
+        sell_price = st.number_input("Sell price", value=selected_product["sell_price"])
+        quantity = st.number_input("Quantity", value=selected_product["quantity"])
+        buy_price = st.number_input("Buy price", value=selected_product["buy_price"])
+        tags = st.multiselect("Categories", ["Flower", "Tree", "Object", "Other", "Manure"], selected_product["tags"])
+        image_show = show_photo(selected_product["image_id"])
+        if image_show:
+            st.image(image_show)
+        image = st.file_uploader("Image", type=['jpg', 'jpeg', 'png', 'JPG', 'JPEG', 'PNG'])
+        
+        if st.button("Update Product"):
+            updated_product = Product(
+                name=name,
+                description=description,
+                sell_price=sell_price,
+                quantity=quantity,
+                buy_price=buy_price,
+                tags=tags
+            )
+
+            if image is None:
+                image_id = selected_product["image_id"]
+            else:
+                updated_product.add_product_image(image)
+                
+            response = requests.put(f"{api_url}/products/{selected_product_id}", json=updated_product.dict())
+            
+            if response.status_code == 200:
+                st.success("Product updated successfully")
+            else:
+                st.error("Failed to update product")
