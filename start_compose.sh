f3d3ab25713118649a657a58804987ef0e2fa5e4
Kacper Jarzyna <kjarzyn001@gmail.com>
2024-05-30 16:23:55 +0200

    chatbob init

--- start_compose.sh
+++ start_compose.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #  polecenie Docker Compose dla bazy danych
-docker compose -f app/databases/docker-compose.yml up
+docker compose -f app/databases/docker-compose.yml up -d --build
 
 #  polecenie Docker Compose dla logowania i rejestracji
-docker compose -f app/login_register/docker-compose.yml up
+docker compose -f app/login_register/docker-compose.yml up -d --build
 
 # polecenie Docker Compose dla forntendu
-docker compose -f app/frontend/docker-compose.yml up
+docker compose -f app/frontend/docker-compose.yml up -d --build
 
 # przykładowy Docker Compose dla dodawania produktów
-docker compose -f /app/controllers/admin/add_product/docker-compose.yml up
+docker compose -f app/controllers/admin/add_product/docker-compose.yml up -d --build
 
 # przykładowy Docker Compose dla dodawania produktów
-docker compose -f /app/controllers/admin/update_product/docker-compose.yml up
+docker compose -f app/controllers/admin/update_product/docker-compose.yml up -d --build
