3ddfeb3723297fc5ea04b2c4a9e4a51f7be76f3a
Łukasz Faruga <lukasz.faruga1@gmail.com>
2024-05-31 20:30:53 +0200

    status history

--- app/databases/api/src/models/address.py
+++ app/databases/api/src/models/address.py
@@ -5,16 +5,16 @@ from typing import Optional
 
 class Address(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
+    name: str
     street: str
     city: str
-    state: str
-    country: str
+    postal_code: str
 
     user: Optional["User"] = Relationship(back_populates="address")
 
 
 class AddressUpdate(BaseModel):
+    name: str | None = None
     street: str | None = None
     city: str | None = None
-    state: str | None = None
-    country: str | None = None
+    postal_code: str | None = None
--- app/databases/api/src/models/order.py
+++ app/databases/api/src/models/order.py
@@ -1,13 +1,16 @@
 from sqlmodel import SQLModel, Field
 from enum import Enum
 from pydantic import BaseModel
+from datetime import datetime
 
 
 class Order(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     cart_id: int = Field(foreign_key="cart.id")
     user_id: int = Field(foreign_key="user.id")
-    date: str
+    address_id: int = Field(foreign_key="address.id")
+    date: str = Field(
+        default_factory=lambda: datetime.now().strftime("%d-%m-%Y %H:%M:%S"))
     status: str
     total_amount: float
 
