06bb9a1d5daf1061534c7bcb42ac65a5c0793f53
Kacper Jarzyna <kjarzyn001@gmail.com>
2024-06-09 22:25:51 +0200

    api key in .env

--- app/processes/chatbot/chatbot.py
+++ app/processes/chatbot/chatbot.py
@@ -2,10 +2,11 @@ from fastapi import FastAPI, HTTPException
 from fastapi.responses import StreamingResponse
 from openai import AsyncOpenAI
 from dataclasses_custom.chatbot_class import Chatbot, PromptFiller
+import os
 
 app = FastAPI()
 bot = Chatbot(data = {
-    "openai_client":  AsyncOpenAI(api_key="sk-proj-8j1NyU5gvA4njucbGG3uT3BlbkFJxBLHcRn4z7futfYb4sgb")
+    "openai_client":  AsyncOpenAI(api_key=os.environ.get('USERPROFILE'))
 })
 filler = PromptFiller()
 
